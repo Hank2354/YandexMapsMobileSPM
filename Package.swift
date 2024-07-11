7bc15f4918e42d55d439170f7e21ccc2fa92fe27
Vladislav <97443104+Hank2354@users.noreply.github.com>
2024-07-11 12:39:50 +0300

    update package swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YandexMapsMobileSPM",
-            url: "https://github.com/Hank2354/YandexMapsMobileSPM/releases/download/1.0.0/YandexMapsMobileSPM.xcframework.zip",
+            url: "https://github.com/Hank2354/YandexMapsMobileSPM/releases/download/4.6.1/YandexMapsMobileSPM.xcframework.zip",
             checksum: "0732383102bdfec750cb404a189daf07f8a7770d79532b02786f50311f217b5a"
         )
     ]
